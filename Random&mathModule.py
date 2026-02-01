f9fa0843e582c36876bcc0e569f08449d37f7293
sabirkhan20 <sabbirkhan082005@gmail.com>
2026-02-01 17:38:11 +0530

    Add files via upload

---
+++ Random&mathModule.py
@@ -0,0 +1,25 @@
+# import math
+
+# a = math.cbrt(64)
+# a = math.nextafter(34,10)
+# a = math.comb(12,2)
+# a = math.exp(2)
+# a = math.ceil(9.1)
+# a = math.floor(9.9)
+# a = math.sumprod([1,2,3],[5,6,7])
+# a =math.pow(2,3)
+# print(a)
+# import random
+
+# li =["apple","banana","grapes"]
+
+# a = random.random()
+# a = random.randint(1000,9999)
+# a = random.randrange(1000,10000)
+# a = random.choice(li)
+# print(a)
+# import random
+# otp = random.randint(100000,999999)
+# print("Your 6 dogoit OTP is :" ,otp)
+
+

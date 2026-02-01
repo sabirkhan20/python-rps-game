6a2b76f3d96647d7812cbb7f11267e21e43f4aba
sabirkhan20 <sabbirkhan082005@gmail.com>
2026-02-01 17:37:15 +0530

    Add files via upload

---
+++ OSModule.py
@@ -0,0 +1,35 @@
+# import os
+
+# a = os.getcwd()
+# a = os.curdir
+# a = os.listdir()
+# print(a)
+
+# # os.mkdir("SQL")
+# os.makedirs("SQL1/Connection")
+
+# import os 
+
+# a= os.getcwd()
+# a= os. curdir
+# a=os. listdir()
+# print(a)
+
+# import datetime
+
+# a = datetime.datetime.now()
+# # a = datetime.date(2025,11,12)
+# a = a.date()
+# # # a = a.time()
+# print(a)
+
+# import datetime
+
+# a= datetime.datetime.now()
+# # a= a.date()
+# # a=a.time()
+# print(a)
+
+
+
+

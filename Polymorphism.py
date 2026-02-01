969ac7ded5a873a3f9fa35221e634ab9fb1ee5c8
sabirkhan20 <sabbirkhan082005@gmail.com>
2026-02-01 17:36:47 +0530

    Add files via upload

---
+++ Polymorphism.py
@@ -0,0 +1,188 @@
+# compile time polymorphism // method overloading
+# class Calculation:
+
+#     # //method overloading
+#     def sum(self,a,b,c=0,d=0,e=0):
+#         print(a+b+c+d+e)
+
+
+# obj = Calculation()
+# obj.sum(34,34)
+# obj.sum(23,23.23)
+# obj.sum(1,2,3)
+
+# class calculation:
+#     def sum(self,a,b,c,d):
+#         print(a+b+c+d)
+# obj=(calculation) ()
+# obj.sum(2,3,4,5)
+
+
+# class Car:
+#     def __init__(self, brand, model):
+#         self.brand = brand
+#         self.model = model
+
+#     def display_info(self):
+#         print(f"This car is a {self.brand} {self.model}.")
+
+# # Creating an object
+# my_car = Car("Toyota", "Corolla")
+# my_car.display_info()
+
+
+# class truck:
+#     def __init__(self,brand,model):
+#         self.brand = brand
+#         self.model = model
+#         self.door= 0
+
+#     def display__info(self):
+#         print(f"this truck is a {self.brand} {self.model}.")
+
+
+# obj = truck ("tata","ace")
+# obj.display__info()
+
+
+# class car :
+#       def __init__(self,brand,model,colour):
+#         self.brand= brand
+#         self.model=model
+#         self.colour=colour
+
+#       def display__info(self):
+#           print(f"this car details are {self.brand}{self.model} {self.colour}")
+          
+
+# obj = car ("maruti","dzire","purple")
+# obj.display__info()
+
+
+
+# class hotel:
+#     def __init__(self.loctaion,city,colour):
+#         self.location=location
+#         self.city=city
+#         self.colour=colour
+
+#     def display__info(self):
+#         print(f"this hotel details are {self.location}{self.city}{self.colour}")
+
+# obj=hotel("bandra","mumbai","white")
+# obj.display__info()
+
+
+
+ 
+# class  hostel:
+#     def __init__(self.nameofhostel,guardname,noofromms):
+#         self.nameofhostel="nameofhostel"
+#         self.guardname-"guardname"
+#         self.noofrooms="noofroms"
+
+#     def display__info(self):
+#         print(f"hostel details are as follows")
+
+# obj= hostel("narmda","rahul",67)
+# obj.display__info
+
+
+
+# class hospital:
+#     def __init__(self,nameofhospital,locationofhospital,nameofdoctor):
+#         self.nameofhospital="nameofhospital"
+#         self.locationofhospital="locationofhospital"
+#         self.nameofdoctor="nameofdoctor"
+
+#     def printdetails__info(self):
+#         print(f"the hospital details are as follows")
+
+# obj=hospital("apollo","acrossthecountry","rahul")
+# obj.printdetails__info()
+
+# class hospital:
+#     def __init__(self, nameofhospital, locationofhospital, nameofdoctor):
+#         self.nameofhospital = nameofhospital
+#         self.locationofhospital = locationofhospital
+#         self.nameofdoctor = nameofdoctor
+
+#     def printdetails_info(self):
+#         print("The hospital details are as follows:")
+#         print(f"Hospital Name: {self.nameofhospital}")
+#         print(f"Location: {self.locationofhospital}")
+#         print(f"Doctor: {self.nameofdoctor}")
+
+# obj = hospital("Apollo", "Across the country", "Rahul")
+# obj.printdetails_info()
+
+# //Run time polymorphism // method overriding
+# class Animal:
+
+#     def sound(self):
+#         print("Sound")
+
+# class Dog(Animal):
+
+#     def sound(self):
+#         print("Bark")
+
+# obj = Dog()
+# obj.sound()
+
+
+
+# class player:
+    
+
+#     def playing(self):
+#         print("good")
+    
+# class team(player):
+#      def playing(self):
+#          print("good")
+        
+# obj = team
+# obj.playing()
+
+
+# a=45
+# b=56
+# sum=(a+b)
+# print(sum)
+
+# a=5
+# b=4.4
+# sum=int(a+b)
+# print(sum)
+
+
+# a=9
+# volofcube=(a**2)
+# print(volofcube)
+
+
+# b=2
+# h=4
+# areaofrectangle=(1/2*b*h)
+# print(areaofrectangle)
+
+
+# import Custommodule as cm;
+# from Custommodule import *
+
+# # cm.sum(23,23)
+
+# mul(23,2)
+# sum(23,34)
+
+# weuse*foruse all()
+
+
+
+
+
+
+
+
+
